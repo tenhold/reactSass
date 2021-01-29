29319cd69fd02ec0d71683ac087d19d5607aa0e6
tenhold <tenhold@gmail.com>
2021-01-29 15:06:16 -0600

    (add) light/dark mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './sass/style.scss';
 
 export default function App() {
+  const [theme, setTheme] = useState(true);
+
+  const handleChange = (): void => {
+    setTheme(!theme);
+    if (theme) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+  };
   return (
-    <div>
-      <h1>Hello, world 2x2!</h1>
-      <h2>Hi Jon!!!!</h2>
+    <div className='container'>
+      <h1>Light / Dark Mode</h1>
+      <div className='toggle-container'>
+        <input
+          type='checkbox'
+          id='switch'
+          name='theme'
+          onChange={handleChange}
+        />
+        <label htmlFor='switch'>Toggle</label>
+      </div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius mollitia
+        doloribus illo tenetur aliquam, optio, accusantium dolorem libero
+        consequatur a natus voluptatem sapiente veritatis quibusdam ipsum porro
+        id maiores? Minima!
+      </p>
     </div>
+
+    // <div className='btn'>
+    //   <button onClick={handleClick}>Change background</button>
+    // </div>
   );
 }
